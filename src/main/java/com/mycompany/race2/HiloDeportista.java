9c9dd2028e4779092c75beb093c6a69b861bf403
EDGAR ALEXANDER MARTINEZ GAITAN <ealexandermartinez@ucundinamarca.edu.co>
2019-04-10 21:09:58 -0500

    Se crearon las clases y metodos necesarios del proyecto

--- src/main/java/com/mycompany/race2/HiloDeportista.java
+++ src/main/java/com/mycompany/race2/HiloDeportista.java
@@ -40,85 +40,104 @@ public class HiloDeportista extends Thread {
     }
 
     @Override
-    public void run() {
+     public void run() {
 
         while (pasost < 100) {
+
+            if (deportista.getPasos() > pasost) {
+                synchronized (deportista) {
+                    try {
+                        System.out.println("El jugador " + deportista.getNombreDeportista() + " esperando");
+                        deportista.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Deportista.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
             if (deportista.getPuntoPartida() == 1) {
 
-                while (deportista.getPasos() < 33) {
-
+                while (deportista.getPasos() < 33 && pasost < 33) {
                     Utilitarios uti = new Utilitarios();
 
                     int numero = uti.random();
                     deportista.setPasos(deportista.getPasos() + numero);
-                    pasost = pasost + numero;
-                    synchronized (deportista) {
-                        if (deportista.getPasos() >= 33 && pasost >= 33) {
-                            deportista.setPasos(33);
-                            setPasost(33);
+                    setPasost(pasost + numero);
 
+                    if (deportista.getPasos() >= 33) {
+                        deportista.setPasos(33);
+                    }
+                    System.out.println("El jugador " + deportista.getNombreDeportista() + " va en la posicion" + deportista.getPasos());
+
+                }
+                synchronized (deportista) {
+                    deportista.notifyAll();
+                }
+
+            }
+
+        }
+        if (deportista.getPasos() >= pasost) {
+            synchronized (deportista) {
+                try {
+                    System.out.println("El jugador " + deportista.getNombreDeportista() + " esperando");
+                    deportista.wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Deportista.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        if (deportista.getPuntoPartida() == 2 && pasost < 66) {
+
+            while (deportista.getPasos() < 66) {
+                Utilitarios uti = new Utilitarios();
+                synchronized (deportista) {
+                    int numero = uti.random();
+                    deportista.setPasos(deportista.getPasos() + numero);
+                    pasost = pasost + numero;
+
+                    if (deportista.getPasos() >= 66) {
+                        deportista.setPasos(66);
+                    }
+                    System.out.println("El jugador " + deportista.getNombreDeportista() + " va en la posicion" + deportista.getPasos());
+                    deportista.notifyAll();
+                }
+
+            }
+
+        }
+        if (deportista.getPasos() > pasost) {
+            synchronized (deportista) {
+                try {
+                    System.out.println("El jugador " + deportista.getNombreDeportista() + " esperando");
+                    deportista.wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Deportista.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (deportista.getPuntoPartida() == 3 && pasost >= 66 && pasost < 100) {
+
+                while (deportista.getPasos() < 100) {
+                    Utilitarios uti = new Utilitarios();
+                    synchronized (deportista) {
+                        int numero = uti.random();
+                        deportista.setPasos(deportista.getPasos() + numero);
+                        pasost = pasost + numero;
+                        if (deportista.getPasos() >= 100) {
+                            deportista.setPasos(100);
                         }
                         System.out.println("El jugador " + deportista.getNombreDeportista() + " va en la posicion" + deportista.getPasos());
                         deportista.notifyAll();
                     }
                 }
-            } else if (deportista.getPuntoPartida() == 2) {
-                while (true) {
-                    synchronized (deportista) {
-                        try {
-                            deportista.wait();
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(HiloDeportista.class.getName()).log(Level.SEVERE, null, ex);
-                        }
 
-                        while (deportista.getPasos() <=66) {
-
-                            Utilitarios uti = new Utilitarios();
-
-                            int numero = uti.random();
-                            deportista.setPasos(deportista.getPasos() + numero);
-                            pasost = pasost + numero;
-                            if (deportista.getPasos() >= 66 && pasost >= 66) {
-                                deportista.setPasos(66);
-                                setPasost(66);
-
-                            }
-                            System.out.println("El jugador " + deportista.getNombreDeportista() + " va en la posicion" + deportista.getPasos());
-                            deportista.notify();
-                        }
-                    }
-                }
-            } else {
-
-                synchronized (deportista) {
-                    while (true) {
-                        try {
-                            deportista.wait();
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(HiloDeportista.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        if (deportista.getPasos() >= 66) {
-                            while (deportista.getPasos() < 100) {
-
-                                Utilitarios uti = new Utilitarios();
-
-                                int numero = uti.random();
-                                deportista.setPasos(deportista.getPasos() + numero);
-                                pasost = pasost + numero;
-                                if (deportista.getPasos() >= 100 && pasost >= 100) {
-                                    deportista.setPasos(100);
-                                    setPasost(100);
-
-                                }
-                                System.out.println("El jugador " + deportista.getNombreDeportista() + " va en la posicion" + deportista.getPasos());
-
-                            }
-                        }
-
-                    }
-                }
             }
+
         }
+
         System.out.println("termino el jugador" + deportista.getNombreDeportista());
     }
 }
+
+
+
+
